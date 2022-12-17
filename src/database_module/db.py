0fe3bbdc09bbd3760de9af5f760c2ab05212d0ff
stepa <awelijuh@gmail.com>
2022-12-17 12:11:04 +0300

    fix

--- src/database_module/db.py
+++ src/database_module/db.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import sessionmaker
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # yolov5 strongsort root directory
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///" + os.path.join(str(ROOT), 'sqlite.db')
+SQLALCHEMY_DATABASE_URL = "sqlite:///" + os.path.join(str(ROOT), '../../media/sqlite.db')
 
 print(SQLALCHEMY_DATABASE_URL)
 
